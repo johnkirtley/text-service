99d5b144a31316733f67472be9e5d2b3310915c1
John Kirtley <johnkirtley@baici-ap-d2md6m.lan>
2022-10-17 11:54:59 -0700

    Updated Empty QR Code view

--- components/QRCode.jsx
+++ components/QRCode.jsx
@@ -1,33 +1,38 @@
 import Image from 'next/image';
-import { Layout, Space } from 'antd';
+import { Layout, Space, Empty } from 'antd';
 
 const { Content } = Layout;
 
 export default function QRCode({ qrCodes }) {
     return (
-        <>
-            <div>
-            Generated Codes
-            </div>
-            <Content style={{ display: 'grid', margin: '0 2rem', gridTemplateColumns: 'repeat(3, 1fr)', marginBottom: '2rem' }}>
-                {qrCodes.map((codeInfo, idx) => (
-                    <Space key={idx} style={{ justifyContent: 'space-between', marginBottom: '1rem', marginTop: '2rem' }} size="large">
-                        <div
-                            className="qr-code-container"
-                            style={{
-                                display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center', margin: '0 2rem',
-                            }}
-                        >
-                            {codeInfo.name}
-                            <div>
-                                <a href={codeInfo.src} target="_blank" download={`${codeInfo.name}.jpg`} rel="noreferrer">
-                                    <Image layout="intrinsic" width={150} height={150} src={codeInfo.src} alt={codeInfo.name} />
-                                </a>
-                            </div>
-                        </div>
-                    </Space>
-                ))}
-            </Content>
-        </>
+        <div>
+            {qrCodes.length > 0
+                ? (
+                    <div style={{ textAlign: 'center' }}>
+                        <p>
+                            Generated Codes
+                        </p>
+                        <Content style={{ display: 'grid', margin: '0 2rem', gridTemplateColumns: 'repeat(3, 1fr)', marginBottom: '2rem' }}>
+                            {qrCodes.map((codeInfo, idx) => (
+                                <Space key={idx} style={{ justifyContent: 'space-between', marginBottom: '1rem', marginTop: '2rem' }} size="large">
+                                    <div
+                                        className="qr-code-container"
+                                        style={{
+                                            display: 'flex', flexFlow: 'column', justifyContent: 'center', alignItems: 'center', margin: '0 2rem',
+                                        }}
+                                    >
+                                        {codeInfo.name}
+                                        <div>
+                                            <a href={codeInfo.src} target="_blank" download={`${codeInfo.name}.jpg`} rel="noreferrer">
+                                                <Image layout="intrinsic" width={150} height={150} src={codeInfo.src} alt={codeInfo.name} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                </Space>
+                            ))}
+                        </Content>
+                    </div>
+                ) : <Empty description="No Generated Codes" />}
+        </div>
     );
 }
