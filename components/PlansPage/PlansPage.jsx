4a87a376f124a8ec2cf33e9cd6c1d059eeab417c
johnkirtley <kirtleyj16@gmail.com>
2023-03-19 23:56:18 -0700

    plan page built

--- components/PlansPage/PlansPage.jsx
+++ components/PlansPage/PlansPage.jsx
@@ -1,12 +1,54 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { Card, Button } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
 import Link from 'next/link';
+import createCheckoutSession from '../../stripe/createCheckoutSession';
+import usePremiumStatus from '../../stripe/usePremiumStatus';
+import { useAuth } from '../../Context/AuthContext';
+
+import planInfo from './planInfo';
+import styles from './plans.module.css';
 
 export default function PlansPage() {
+    // const [planClicked, setPlanClicked] = useState(false);
+    const { user } = useAuth();
+
+    const isUserPremium = usePremiumStatus(user);
+
+    const handleBilling = (planType) => {
+        // setPlanClicked(true);
+        createCheckoutSession(user.uid, planType);
+    };
     return (
         <div>
             <Link href="/">
-                Go Back Home
+                <div className={styles.goHome}>
+                    <div>
+                        <ArrowLeftOutlined />
+                    </div>
+                    <p>Go Home</p>
+                </div>
             </Link>
-            <div>Plan Info Page</div>
+            <div className={styles.planGrid}>
+                {planInfo.map((plan, id) => (
+                    <Card title={plan.name} key={id}>
+                        <div className={styles.cardBody}>
+                            <div className={styles.cardBody}>
+                                <div className={styles.planPrice}>{plan.price}</div>
+                                <ul>
+                                    {plan.features.map((feature, idx) => (
+                                        <li key={idx} className={styles.feature}>{feature}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div>
+                                {isUserPremium.planName === plan.name ? <Button type="primary" className={styles.planButton} disabled>Current Plan</Button>
+                                    : <Button type="primary" className={styles.planButton} onClick={() => handleBilling(plan.name)}>Select {plan.name} Plan</Button>}
+                            </div>
+                        </div>
+                    </Card>
+                ))}
+            </div>
         </div>
     );
 }
