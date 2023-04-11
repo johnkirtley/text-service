1e42ce43424773f530d893696cbe132301d249f9
John Kirtley <main@Johns-MacBook-Pro.local>
2023-04-10 17:32:32 -0700

    updated plans

--- components/PlansPage/planInfo.js
+++ components/PlansPage/planInfo.js
@@ -1,12 +1,12 @@
 const planInfo = [
     {
-        name: 'silver',
+        name: 'bronze',
         features: ['Order Request Portal', 'SMS Notifications'],
         price: '$19.99',
         id: 'price_1MulMuGYpJAcieX9neiQ7EPs',
     },
     {
-        name: 'bronze',
+        name: 'silver',
         features: ['All Features From Silver', 'Customer Usage Analytics', 'Direct Customer Communication'],
         price: '$29.99',
         id: 'price_1MulNWGYpJAcieX9qzmHsQ5z',
